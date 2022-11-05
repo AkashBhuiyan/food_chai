bb39aa3ff633fac51ae19589432681afe9944f15
Akash <akash.bhuiyan@gmail.com>
2022-11-06 00:27:33 +0600

    google authentication added.

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,9 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:food_chai/home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: ['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final User? user = (await _auth.signInWithCredential(credential)).user;
+      // print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,30 +58,38 @@ class SignIn extends StatelessWidget {
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                const Text('Sign in to continue'),
-                Text(
-                  'Food Chai',
-                  style: TextStyle(fontSize: 50, color: Colors.white, shadows: [
-                    BoxShadow(
-                        blurRadius: 5,
-                        color: Colors.green.shade900,
-                        offset: const Offset(3, 3)),
+                    const Text('Sign in to continue'),
+                    Text(
+                      'Food Chai',
+                      style: TextStyle(
+                          fontSize: 50, color: Colors.white, shadows: [
+                        BoxShadow(
+                            blurRadius: 5,
+                            color: Colors.green.shade900,
+                            offset: const Offset(3, 3)),
+                      ]),
+                    ),
+                    Column(
+                        children: [
+                          SignInButton(
+                            Buttons.Google,
+                            text: "Sign in with Google",
+                            onPressed: () {
+                              _googleSignUp().then((value) =>
+                                  Navigator.of(context).pushReplacement(
+                                      MaterialPageRoute(
+                                          builder: (context) => HomeScreen()),
+                                  ),
+                              );
+                            },
+                          ),
+                        ]),
+                    Column(
+                      children: [
+                        Text("All right reserved AB")
+                      ],
+                    )
                   ]),
-                ),
-                Column(
-                  children: [
-                  SignInButton(
-                    Buttons.Google,
-                    text: "Sign in with Google",
-                    onPressed: () {},
-                  ),
-                ]),
-                Column(
-                  children: [
-                    Text("All right reserved AB")
-                  ],
-                )
-              ]),
             ),
           ],
         ),
