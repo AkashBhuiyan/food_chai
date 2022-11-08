e736e04e7c3fc8e35660b90720d04fe3886fb643
Akash <akash.bhuiyan@egeneration.co>
2022-11-08 12:47:50 +0600

    home screen discount advertise design added.

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      drawer: const Drawer(),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
         title: Text(
@@ -46,7 +46,7 @@ class HomeScreen extends StatelessWidget {
             Container(
               height: 150,
               decoration: BoxDecoration(
-                image: DecorationImage(
+                image: const DecorationImage(
                   fit: BoxFit.cover,
                   image: NetworkImage(
                       'https://www.eitfood.eu/files/_1200x514_crop_center-center_82_line/AdobeStock_102476307.jpeg'),
@@ -54,16 +54,69 @@ class HomeScreen extends StatelessWidget {
                 color: Colors.red,
                 borderRadius: BorderRadius.circular(10),
               ),
-
               child: Row(
                 children: [
-                  Expanded(child: Container(
-                    color: Colors.red,
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 130, bottom: 10),
+                            child: Container(
+                              height: 50,
+                              width: 100,
+                              decoration: const BoxDecoration(
+                                color: Color(0xff39b2b7),
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(50),
+                                  bottomLeft: Radius.circular(50),
+                                ),
+                              ),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 15, top: 5),
+                                child: Text(
+                                  'Food Chai',
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    color: Colors.white,
+                                    shadows: [
+                                      BoxShadow(
+                                        color: Colors.green,
+                                        blurRadius: 3,
+                                        offset: Offset(3, 3),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Text(
+                            '25% Off',
+                            style: TextStyle(
+                                fontSize: 35,
+                                color: Colors.lightGreen,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 17),
+                            child: const Text(
+                              'On All groceries product, Condition apply',
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.black87,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
+                  Expanded(
+                    child: Container(),
                   ),
-                  Expanded(child: Container(
-
-                  ),),
                 ],
               ),
             )
