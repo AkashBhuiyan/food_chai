8062e8137c58fdcb673c48236703e4abb670bf43
Akash <akash.bhuiyan@gmail.com>
2022-12-28 12:15:29 +0600

    Search full design added

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_chai/config/colors.dart';
+import 'package:food_chai/screens/search/search_item.dart';
 
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
@@ -12,6 +13,42 @@ class Search extends StatelessWidget {
         title: Text(
           'Search',
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.menu_rounded),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Items'),
+          ),
+          Container(
+            height: 52,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none
+                ),
+                fillColor: Color(0xffc2c2c2),
+                filled: true,
+                hintText: "Search for item in the store",
+                suffixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SearchItem(),
+          SearchItem(),
+          SearchItem(),
+          SearchItem(),
+          SearchItem(),
+        ],
       ),
     );
   }
